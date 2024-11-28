60905b6fcec8f88cf909b166a90250126862bb98
yuberee <59583743+yuberee@users.noreply.github.com>
2024-11-28 22:37:16 +0100

    Update to scene system

--- game/code/Icon.cs
+++ game/code/Icon.cs
@@ -1,158 +1,160 @@
 ﻿namespace RockPaperScissors;
+using Sandbox;
+using Sandbox.UI;
+using System;
+using System.Collections.Generic;
 
 public enum IconType
 {
-	Rock,
-	Paper,
-	Scissors
+    Rock,
+    Paper,
+    Scissors
 }
 
 public partial class Icon : Label
 {
-	public IconType? Type;
-	public static Dictionary<IconType, List<Icon>> All = new()
-	{
-		{ IconType.Rock, new() },
-		{ IconType.Paper, new() },
-		{ IconType.Scissors, new() }
-	};
-	public IconType Prey => (IconType)((int)(Type.Value - 1) == -1 ? Enum.GetValues( typeof( IconType ) ).Length - 1 : (int)(Type.Value - 1)); // The type before is what this icon can eat - Loops back
-	public IconType Predator => (IconType)((int)(Type.Value + 1) % Enum.GetValues( typeof( IconType ) ).Length); // The type after is what this icon gets eaten by - Loops back
+    public IconType? Type;
+    public static Dictionary<IconType, List<Icon>> All = new()
+    {
+        { IconType.Rock, new() },
+        { IconType.Paper, new() },
+        { IconType.Scissors, new() }
+    };
+    public IconType Prey => (IconType)((int)(Type.Value - 1) == -1 ? Enum.GetValues(typeof(IconType)).Length - 1 : (int)(Type.Value - 1)); // The type before is what this icon can eat - Loops back
+    public IconType Predator => (IconType)((int)(Type.Value + 1) % Enum.GetValues(typeof(IconType)).Length); // The type after is what this icon gets eaten by - Loops back
 
-	public Vector2 PixelPosition
-	{
-		get => new Vector2( Style.Left.Value.Value / 100f * Screen.Width, Style.Top.Value.Value / 100 * Screen.Height );
-		set
-		{
-			Style.Left = Length.Fraction( value.x / Screen.Width );
-			Style.Top = Length.Fraction( value.y / Screen.Height );
-		}
-	}
-	public float PixelSize => 40f / RockPaperScissors.Instance.Zoom * ScaleToScreen;
-	public Icon Chasing { get; set; } = null;
+    public Vector2 PixelPosition
+    {
+        get => new Vector2(Style.Left.Value.Value / 100f * Screen.Width, Style.Top.Value.Value / 100 * Screen.Height);
+        set
+        {
+            Style.Left = Length.Fraction(value.x / Screen.Width);
+            Style.Top = Length.Fraction(value.y / Screen.Height);
+        }
+    }
+    public float PixelSize => 40f / UI.Zoom * ScaleToScreen;
+    public Icon Chasing { get; set; } = null;
 
-	public Icon( IconType type )
-	{
-		SetType( type );
-		Style.Left = Length.Fraction( Game.Random.Float( 1f ) );
-		Style.Top = Length.Fraction( Game.Random.Float( 1f ) );
-	}
+    public Icon(IconType type)
+    {
+        SetType(type);
+        Style.Left = Length.Fraction(Game.Random.Float(1f));
+        Style.Top = Length.Fraction(Game.Random.Float(1f));
+    }
 
-	public void SetType( IconType type )
-	{
-		if ( Type != null )
-			All[Type.Value].Remove( this );
-		Type = type;
-		All[type].Add( this );
+    public void SetType(IconType type)
+    {
+        if (Type != null)
+            All[Type.Value].Remove(this);
+        Type = type;
+        All[type].Add(this);
 
-		Text = Type switch
-		{
-			IconType.Rock => "🗿",
-			IconType.Paper => "📜",
-			IconType.Scissors => "✂️",
-			_ => String.Empty
-		};
-	}
+        Text = Type switch
+        {
+            IconType.Rock => "🗿",
+            IconType.Paper => "📜",
+            IconType.Scissors => "✂️",
+            _ => String.Empty
+        };
+    }
 
-	public void PlaySound()
-	{
-		if ( Type == null ) return;
+    public void PlaySound()
+    {
+        if (Type == null) return;
 
-		var position = PixelPosition;
-		Sound.FromScreen( $"sounds/{Type.ToString().ToLower()}.sound", position.x * ScaleFromScreen / Screen.Width, position.y * ScaleFromScreen / Screen.Width );
-	}
+        var position = PixelPosition;
+        Sound.Play($"sounds/{Type.ToString().ToLower()}.sound");
+    }
 
-	[Event.Client.Frame]
-	void computeAI()
-	{
-		if ( Type == null ) return;
+    public void OnTick()
+    {
+        if (Type == null) return;
 
-		Style.ZIndex = (int)PixelPosition.y; // Sort their ZIndex based on their vertical position to remove annoying clipping
+        Style.ZIndex = (int)PixelPosition.y; // Sort their ZIndex based on their vertical position to remove annoying clipping
 
-		var velocity = Vector2.Zero;
-		var currentPosition = PixelPosition;
+        var velocity = Vector2.Zero;
+        var currentPosition = PixelPosition;
 
-		// Run from predators
+        // Run from predators
 
-		foreach ( var predator in Icon.All[Predator] )
-		{
-			var predatorPosition = predator.PixelPosition;
+        foreach (var predator in Icon.All[Predator])
+        {
+            var predatorPosition = predator.PixelPosition;
 
-			if ( currentPosition.Distance( predatorPosition ) <= PixelSize * 2f )
-			{
-				velocity = (currentPosition - predatorPosition).Normal * Time.Delta * (40f / RockPaperScissors.Instance.Zoom) * ScaleToScreen;
-				break;
-			}
-		}
+            if (currentPosition.Distance(predatorPosition) <= PixelSize * 2f)
+            {
+                velocity = (currentPosition - predatorPosition).Normal * Time.Delta * (40f / UI.Zoom) * ScaleToScreen;
+                break;
+            }
+        }
 
-		// Push same icons
+        // Push same icons
 
-		var pushOffset = Vector2.Zero;
+        var pushOffset = Vector2.Zero;
 
-		foreach ( var ally in Icon.All[Type.Value] )
-		{
-			if ( this == ally ) continue;
+        foreach (var ally in Icon.All[Type.Value])
+        {
+            if (this == ally) continue;
 
-			var allyPosition = ally.PixelPosition;
+            var allyPosition = ally.PixelPosition;
 
-			if ( currentPosition.Distance( allyPosition ) <= PixelSize * 1f )
-			{
-				pushOffset = (currentPosition - allyPosition).Normal * Time.Delta * (15f / RockPaperScissors.Instance.Zoom) * ScaleToScreen;
-				break;
-			}
-		}
+            if (currentPosition.Distance(allyPosition) <= PixelSize * 1f)
+            {
+                pushOffset = (currentPosition - allyPosition).Normal * Time.Delta * (15f / UI.Zoom) * ScaleToScreen;
+                break;
+            }
+        }
 
-		// Chase preys
+        // Chase preys
 
-		var chasingDistance = 0f;
-		var chasingPosition = Vector2.Zero;
+        var chasingDistance = 0f;
+        var chasingPosition = Vector2.Zero;
 
-		if ( Chasing != null )
-		{
-			if ( Chasing.Type.Value != Prey )
-				Chasing = null;
-			else
-			{
-				chasingPosition = Chasing.PixelPosition;
-				chasingDistance = currentPosition.Distance( chasingPosition );
-			}
-		}
+        if (Chasing != null)
+        {
+            if (Chasing.Type.Value != Prey)
+                Chasing = null;
+            else
+            {
+                chasingPosition = Chasing.PixelPosition;
+                chasingDistance = currentPosition.Distance(chasingPosition);
+            }
+        }
 
-		foreach ( var prey in Icon.All[Prey] )
-		{
-			var preyPosition = prey.PixelPosition;
-			var preyDistance = currentPosition.Distance( preyPosition );
-			if ( Chasing != null )
-			{
-				if ( preyDistance < chasingDistance )
-				{
-					Chasing = prey;
-					chasingDistance = preyDistance;
-					chasingPosition = preyPosition;
-				}
-			}
-			else
-			{
-				Chasing = prey;
-				chasingDistance = preyDistance;
-				chasingPosition = preyPosition;
-			}
-		}
+        foreach (var prey in Icon.All[Prey])
+        {
+            var preyPosition = prey.PixelPosition;
+            var preyDistance = currentPosition.Distance(preyPosition);
+            if (Chasing != null)
+            {
+                if (preyDistance < chasingDistance)
+                {
+                    Chasing = prey;
+                    chasingDistance = preyDistance;
+                    chasingPosition = preyPosition;
+                }
+            }
+            else
+            {
+                Chasing = prey;
+                chasingDistance = preyDistance;
+                chasingPosition = preyPosition;
+            }
+        }
 
-		if ( Chasing != null )
-		{
-			velocity += (chasingPosition - currentPosition).Normal * Time.Delta * (50f / RockPaperScissors.Instance.Zoom) * ScaleToScreen;
+        if (Chasing != null)
+        {
+            velocity += (chasingPosition - currentPosition).Normal * Time.Delta * (50f / UI.Zoom) * ScaleToScreen;
 
-			if ( chasingDistance <= PixelSize )
-			{
-				Chasing.SetType( Type.Value );
-				PlaySound();
-			}
-		}
+            if (chasingDistance <= PixelSize)
+            {
+                Chasing.SetType(Type.Value);
+                PlaySound();
+            }
+        }
 
-		// Apply new position
-		var jiggle = new Vector2( Game.Random.Float( -1, 1 ), Game.Random.Float( -1, 1 ) ) * (0.5f / RockPaperScissors.Instance.Zoom) * Time.Delta * 60f * ScaleToScreen;
-		PixelPosition += velocity.Clamp( 0.5f / RockPaperScissors.Instance.Zoom * Time.Delta * -60f, 0.5f / RockPaperScissors.Instance.Zoom * Time.Delta * 60f ) + jiggle + pushOffset;
-
-	}
+        // Apply new position
+        var jiggle = new Vector2(Game.Random.Float(-1, 1), Game.Random.Float(-1, 1)) * (0.5f / UI.Zoom) * Time.Delta * 60f * ScaleToScreen;
+        PixelPosition += velocity.Clamp(0.5f / UI.Zoom * Time.Delta * -60f, 0.5f / UI.Zoom * Time.Delta * 60f) + jiggle + pushOffset;
+    }
 }
